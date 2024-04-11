adcd60253df9dbbf90984886a89e065c09308f65
unknown <159421702+swandrn@users.noreply.github.com>
2024-04-12 01:41:11 +0200

    Created function to scrape a table element

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -22,4 +22,32 @@ function scrapeCountries() {
     });
 }
 
+function scrapeTable(){
+    let resArray = new Array();
+    receiveAjax('./php/proxy.php').then(function (rawData){
+        let parser = new DOMParser();
+        let htmlDoc = parser.parseFromString(rawData.responseText, 'text/html');
 
+        let tableHeaders = htmlDoc.getElementsByTagName('th');
+        let tableHeadersContent = new Array();
+        for(let i = 0; i < tableHeaders.length; ++i){
+            tableHeadersContent.push(tableHeaders[i].textContent.trim());
+        }
+        resArray.push(tableHeadersContent);
+
+        let tableRows = htmlDoc.getElementsByTagName('tr');
+        for(let i = 0; i < tableRows.length; ++i){
+            let tableData = tableRows[i].getElementsByTagName('td');
+            let tableDataContent = new Array();
+            if(Array.isArray(tableData) || tableData.length){
+                for (let j = 0; j < tableData.length; j++) {
+                    tableDataContent.push(tableData[j].textContent.trim());
+                }
+                resArray.push(tableDataContent);
+            }
+        }
+    });
+    return resArray;
+}
+
+console.log(scrapeTable());
