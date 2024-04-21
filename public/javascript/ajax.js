5de1ea7b40661c4386aea92ad195dee339b232dc
unknown <159421702+swandrn@users.noreply.github.com>
2024-04-21 15:13:13 +0200

    Created toggleInputs function to display or hide parameters of the scrape request. Also allowed createTable() to handle 1D or 2D arrays

--- public/javascript/ajax.js
+++ public/javascript/ajax.js
@@ -3,12 +3,14 @@ const scrapeAPI = './api/scrape-request';
 const scrapeRequestWrapper = document.getElementById('scrape-request-wrapper');
 const urlInput = document.getElementById('url-input');
 const selectorInput = document.getElementById('selector-input');
+const tagToScrapeInput = document.getElementById('tag-to-scrape-input');
 const expectedUrlInput = document.getElementById('expected-url-input');
 const hasAjaxInput = document.getElementById('has-ajax-input');
 const scrapeButton = document.getElementById('scrape-button');
+const labels = document.getElementsByTagName('label');
 
-async function getScrapedData(requestUrl, urlToScrape, selector, expectedUrl, hasAjax){
-    let params = `urlToScrape=${urlToScrape}&selector=${selector}&expectedUrl=${expectedUrl}&hasAjax=${hasAjax}`;
+async function getScrapedData(requestUrl, urlToScrape, tagToScrape, selector, expectedUrl, hasAjax){
+    let params = `urlToScrape=${urlToScrape}&tagToScrape=${tagToScrape}&selector=${selector}&expectedUrl=${expectedUrl}&hasAjax=${hasAjax}`;
     return new Promise(function (resolve, reject){
         const request = new XMLHttpRequest();
         request.open('POST', requestUrl);
@@ -22,36 +24,111 @@ async function getScrapedData(requestUrl, urlToScrape, selector, expectedUrl, ha
     });
 }
 
-async function createTable(array){
+async function createTable(array) {
     const table = document.createElement('table');
     const tbody = document.createElement('tbody');
-    for(let row = 0; row < array.length; ++row){
-        let tr = document.createElement('tr');
-        if(array[row]?.length){
-            for(let col = 0; col < array[row].length; ++col){
+
+    //2D array
+    if (Array.isArray(array[0])) {
+        for (let row = 0; row < array.length; ++row) {
+            let tr = document.createElement('tr');
+            for (let col = 0; col < array[row].length; ++col) {
                 let td = document.createElement('td');
                 td.textContent = array[row][col];
                 tr.append(td);
             }
             tbody.append(tr);
         }
+        table.append(tbody);
+        return table;
     }
-    table.append(tbody);
-    return table;
+
+    //1D array
+    if(!Array.isArray(array[0])){
+        let tr = document.createElement('tr');
+        for(let col = 0; col < array.length; ++col){
+            let td = document.createElement('td');
+            td.textContent = array[col];
+            tr.append(td);
+        }
+        tbody.append(tr);
+        table.append(tbody);
+        return table;
+    }
+
+    return 'Element is not a table';
+}
+
+function toggleInputs(event){
+    switch (event.target.value) {
+        case 'table':
+            for(let label of labels){
+                if(label.htmlFor == expectedUrlInput.id){
+                    label.classList.add('hidden');
+                    expectedUrlInput.classList.add('hidden');
+                }
+                if(label.htmlFor == hasAjaxInput.id){
+                    label.classList.add('hidden');
+                    hasAjaxInput.classList.add('hidden');
+                }
+            }
+            break;
+        case 'text':
+            for(let label of labels){
+                if(label.htmlFor == expectedUrlInput.id){
+                    label.classList.add('hidden');
+                    expectedUrlInput.classList.add('hidden');
+                }
+                if(label.htmlFor == hasAjaxInput.id){
+                    label.classList.add('hidden');
+                    hasAjaxInput.classList.add('hidden');
+                }
+            }
+            break;
+        case 'anchor':
+            for(let label of labels){
+                if(label.htmlFor == expectedUrlInput.id){
+                    label.classList.remove('hidden');
+                    expectedUrlInput.classList.remove('hidden');
+                }
+                if(label.htmlFor == hasAjaxInput.id){
+                    label.classList.remove('hidden');
+                    hasAjaxInput.classList.remove('hidden');
+                }
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 async function main(){
     let urlValue = urlInput.value;
+    let tagToScrapeValue = tagToScrapeInput.value;
     let selectorValue = selectorInput.value;
     let hasAjaxValue = hasAjaxInput.checked ? 'true' : 'false';
     let expectedUrlValue = expectedUrlInput.value;
 
-    let elements = await getScrapedData(scrapeAPI, urlValue, selectorValue);
-    let parsedElements = JSON.parse(elements.responseText);
+    let elements = await getScrapedData(scrapeAPI, urlValue, tagToScrapeValue, selectorValue, hasAjaxValue, expectedUrlValue);
+    let parsedElement = JSON.parse(elements.responseText);
+    let elementToDisplay;
 
-    let table = await createTable(parsedElements);
+    console.log(parsedElement[0]);
 
-    scrapeRequestWrapper.append(table);
+    //If parsedElement is an array
+    if(Array.isArray(parsedElement)){
+        elementToDisplay = await createTable(parsedElement);
+    }
+
+    //If parsedElement is not an array
+    if(!Array.isArray(parsedElement)){
+        elementToDisplay = await createTable(parsedElement);
+    }
+    
+    scrapeRequestWrapper.append(elementToDisplay);
 }
 
 scrapeButton.addEventListener('click', main);
+tagToScrapeInput.addEventListener('change', function(event){
+    toggleInputs(event);
+});
