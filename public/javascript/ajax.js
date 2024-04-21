93b8e0dac12eed364027bff94683e1a9b49805a0
unknown <159421702+swandrn@users.noreply.github.com>
2024-04-21 17:44:10 +0200

    Added a way to differentiate arrays of json objets from arrays of arrays

--- public/javascript/ajax.js
+++ public/javascript/ajax.js
@@ -28,35 +28,63 @@ async function createTable(array) {
     const table = document.createElement('table');
     const tbody = document.createElement('tbody');
 
-    //2D array
-    if (Array.isArray(array[0])) {
+    // True for array
+    if (Object.prototype.toString.call(array[0]) === '[object Array]') {
+        //2D array
+        if (Array.isArray(array[0])) {
+            for (let row = 0; row < array.length; ++row) {
+                let tr = document.createElement('tr');
+                for (let col = 0; col < array[row].length; ++col) {
+                    let td = document.createElement('td');
+                    td.textContent = array[row][col];
+                    tr.append(td);
+                }
+                tbody.append(tr);
+            }
+            table.append(tbody);
+            return table;
+        } // True for JSON objects
+    } else if (Object.prototype.toString.call(array[0]) === '[object Object]') {
+        const table = document.createElement('table');
+        const tbody = document.createElement('tbody');
+        const keys = Object.keys(array[0]);
+
+        //Append table headers
+        let tr = document.createElement('tr');
+        for (let key of keys) {
+            let th = document.createElement('th');
+            th.textContent = key;
+            tr.append(th);
+        }
+        tbody.append(tr);
+
         for (let row = 0; row < array.length; ++row) {
             let tr = document.createElement('tr');
-            for (let col = 0; col < array[row].length; ++col) {
+            for (let i = 0; i < keys.length; ++i) {
                 let td = document.createElement('td');
-                td.textContent = array[row][col];
+                td.textContent = array[row][keys[i]];
                 tr.append(td);
             }
             tbody.append(tr);
         }
         table.append(tbody);
         return table;
-    }
-
-    //1D array
-    if(!Array.isArray(array[0])){
-        let tr = document.createElement('tr');
-        for(let col = 0; col < array.length; ++col){
-            let td = document.createElement('td');
-            td.textContent = array[col];
-            tr.append(td);
+    } else {
+        //1D array
+        if (!Array.isArray(array[0])) {
+            let tr = document.createElement('tr');
+            for (let col = 0; col < array.length; ++col) {
+                let td = document.createElement('td');
+                td.textContent = array[col];
+                tr.append(td);
+            }
+            tbody.append(tr);
+            table.append(tbody);
+            return table;
         }
-        tbody.append(tr);
-        table.append(tbody);
-        return table;
     }
 
-    return 'Element is not a table';
+    return 'Element is not an array';
 }
 
 function toggleInputs(event){
@@ -109,17 +137,15 @@ async function main(){
     let hasAjaxValue = hasAjaxInput.checked ? 'true' : 'false';
     let expectedUrlValue = expectedUrlInput.value;
 
-    let elements = await getScrapedData(scrapeAPI, urlValue, tagToScrapeValue, selectorValue, hasAjaxValue, expectedUrlValue);
+    let elements = await getScrapedData(scrapeAPI, urlValue, tagToScrapeValue, selectorValue, expectedUrlValue, hasAjaxValue);
     let parsedElement = JSON.parse(elements.responseText);
     let elementToDisplay;
-
-    console.log(parsedElement[0]);
 
     //If parsedElement is an array
     if(Array.isArray(parsedElement)){
         elementToDisplay = await createTable(parsedElement);
     }
-
+    
     //If parsedElement is not an array
     if(!Array.isArray(parsedElement)){
         elementToDisplay = await createTable(parsedElement);
