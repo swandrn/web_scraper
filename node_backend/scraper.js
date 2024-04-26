8971e6ee42a2ee5b259cc65fe5db263ffbee9336
unknown <159421702+swandrn@users.noreply.github.com>
2024-04-26 19:04:59 +0200

    Added function to parse robots.txt of domain and respect the allowed and disallowed pages

--- node_backend/scraper.js
+++ node_backend/scraper.js
@@ -234,6 +234,121 @@ async function escapeNumberSelector(numbers){
     return escapedSelectors;
 }
 
+async function parseRobotsTxt(robotsTxt){
+    //INSPIRED BY https://github.com/chrisakroyd/robots-txt-parser/blob/master/src/parser.js
+    const USER_AGENT = 'user-agent';
+    const ALLOW = 'allow';
+    const DISALLOW = 'disallow';
+    const SITEMAP = 'sitemap';
+    const CRAWL_DELAY = 'crawl-delay';
+    const HOST = 'host';
+
+    //Regex to cleanup the file
+    const comments = /#.*$/gm;
+    const whitespace = ' ';
+    const lineEndings = /[\r\n]+/g;
+    const blockSlices = /(\w+-)?\w+:\s\S*/g;
+
+    //Remove comments and whitespaces
+    const removeComments = (rawString) => rawString.replace(comments, '');
+    const removeSpaces = (rawString) => rawString.replace(whitespace, '').trim();
+    const splitOnLines = (string) => string.split(lineEndings);
+
+    const parseBlock = (line) => {
+        const firstColon = line.indexOf(':');
+        return {
+            field: line.slice(0, firstColon).toLowerCase().trim(),
+            value: line.slice(firstColon + 1).trim()
+        };
+    };
+
+    //I don't know why I need to parse disallow block values
+    // const parsePattern = (pattern) => {
+    //     const regexSpecialChars = /[\-\[\]\/\{\}\(\)\+\?\.\\\^\$\|]/g;
+    //     const wildCard = /\*/g;
+    //     const endOfLine = /\\\$$/;
+    //     const flags = 'm';
+
+    //     const regexString = pattern
+    //         .replace(regexSpecialChars, '\\&')
+    //         .replace(wildCard, '.*')
+    //         .replace(endOfLine, '$');
+
+    //     return new RegExp(regexString, flags);
+    // }
+
+    const groupMemberBlock = (value) => ({
+        specificity: value.length,
+        path: value
+    });
+
+    let pureString = removeSpaces(removeComments(robotsTxt));
+    let lines = splitOnLines(pureString);
+
+    const robotsObj = {
+        sitemaps: [],
+    };
+    let agent = '';
+
+    //Parse robots.txt
+    for (let line of lines) {
+        const block = parseBlock(line);
+        switch (block.field) {
+            case USER_AGENT:
+                const blockValue = block.value.toLowerCase();
+                //If user-agent is set
+                if (blockValue !== agent && blockValue.length > 0) {
+                    agent = blockValue;
+                    robotsObj[agent] = {
+                        allow: [],
+                        disallow: [],
+                        crawlDelay: 0
+                    };
+                }
+                break;
+            case ALLOW:
+                if (agent.length > 0 && block.value.length > 0) {
+                    robotsObj[agent].allow.push(groupMemberBlock(block.value));
+                }
+                break;
+            case DISALLOW:
+                if (agent.length > 0 && block.value.length > 0) {
+                    robotsObj[agent].disallow.push(groupMemberBlock(block.value));
+                }
+                break;
+            //Get sitemap
+            case SITEMAP:
+                if (block.value.length > 0) {
+                    robotsObj.sitemaps.push(block.value);
+                }
+                break;
+            case CRAWL_DELAY:
+                if (agent.length > 0) {
+                    robotsObj[agent].crawlDelay = Number.parseInt(block.value, 10);
+                }
+                break;
+            case HOST:
+                //If no host already present, input this one
+                if (!('host' in robotsObj)) {
+                    robotsObj.host = blockValue;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    robotsObj.sitemaps = robotsObj.sitemaps.filter((val, i, s) => s.indexOf(val) === i);
+    return robotsObj;
+}
+
+async function createError(message){
+    return [{
+        dataType: 'error',
+        errorMessage: message
+    }];
+}
+
 async function main() {
     const app = express();
 
@@ -255,6 +370,7 @@ async function main() {
         let hasAjax = req.body.hasAjax == 'true' ? true : false;
         let matches = urlToScrape.match(/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/i);
         let domain = matches && matches[1];
+        let canScrape = true;
         let scrapedData;
 
         const browser = await puppeteer.launch();
@@ -263,7 +379,34 @@ async function main() {
         const userAgent = 'Coll';
         await page.setUserAgent(userAgent);
 
-        try {
+        scrape : try {
+            await page.goto(`https://${domain}/robots.txt`);
+            const robotsTxtContent = await page.evaluate(() => document.body.textContent);
+
+            const robotsTxt = await parseRobotsTxt(robotsTxtContent);
+
+            
+            if(robotsTxt[userAgent]?.length){
+                //Directives specific to this scrape bot
+            } else{
+                //Directives for all bots
+                let disallowedPaths = [];
+                for(let i = 0; i < robotsTxt['*'].disallow.length; ++i){
+                    disallowedPaths.push(robotsTxt['*'].disallow[i].path);
+                }
+
+                for(let i = 0; i < disallowedPaths.length; ++i){
+                    if(urlToScrape.includes(disallowedPaths[i])){
+                        canScrape = false;
+                    }
+                }
+            }
+
+            if(!canScrape){
+                scrapedData = await createError('You cannot scrape this website');
+                break scrape;
+            }
+
             await page.goto(urlToScrape);
             await page.setViewport({ width: 1080, height: 1024 });
 
@@ -285,7 +428,7 @@ async function main() {
                     break;
             }
         } catch (error) {
-            scrapedData = error.message;
+            scrapedData = await createError(error.message);
         }
 
         await browser.close();
@@ -293,7 +436,7 @@ async function main() {
         res.type('json');
         res.json(scrapedData);
     });
-    
+
     app.listen(5000);
 }
 
